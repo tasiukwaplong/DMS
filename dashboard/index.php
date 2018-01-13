a7af117d770e9ea02d612f28cb56fd5b289f702f
Tasiu Kwaplong Saeed <tasiukwaplong@gmail.com>
2018-01-12 22:38:46 -0800

    Front end dev on 11 january 2018

---
+++ dashboard/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Debt Management System</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="well well-lg">DMS 
+  <p class="pull-right">
+    <a href="../api-documentation/"> Api Documentation</a> | <a href="../more/">More </a>| 
+  </p>
+</div>
+<div class="container-fluid">
+
+  <div class="row">
+    <div class="col-sm-12">
+      <center>
+      <div class="dropdown" style="margin-top: 3em;margin-bottom: 10em">
+        
+         <a class="btn btn-primary " href="add-debt/" >Add Debt</a>
+        <br><br><br>
+        <a class="btn btn-primary " href="manage-debt/" >Manage Debt</a>
+ 
+</div>
+</center>
+
+    </div>
+  </div>
+</div>
+
+<div class="well well-sm">
+  <a href="https://www.github.com/tasiukwaplong">By Tasiu</a>
+</div>
+
+    
+</body>
+
+</html>
---
+++ register/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Debt Management System</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="well well-lg">DMS 
+  <p class="pull-right">
+    <a href="api-documentation/"> Api Documentation</a> | <a href="more/">More </a>| 
+  </p>
+</div>
+<div class="container-fluid">
+
+  <div class="row">
+    <div class="col-sm-12">
+      <center>
+      <form style="margin-bottom: 10em;margin-top: 3em">
+
+    <div class="input-group col-xs-4">
+      <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>   
+      <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+    </div>
+    <br>
+    <div class="input-group col-xs-4">
+      <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+      <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+    </div>
+
+    <div class="input-group col-xs-4">
+      <br>
+    <input type="submit" class="btn btn-primary" value="Login" />
+  </div>
+
+  <a href="../">Login here</a>
+  </form>
+</center>
+
+    </div>
+  </div>
+</div>
+
+<div class="well well-sm">
+  <a href="github.com/tasiukwaplong">By Tasiu</a>
+</div>
+
+    
+</body>
+
+</html>
